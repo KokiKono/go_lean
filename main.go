89e80d166acf9124b18f9a745616f9b3699541de
kokikono <kk.k.0421@outlook.jp>
2019-03-10 22:13:42 +0900

    add: 1.1.1.2

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +35,9 @@ func main() {
 			</html>
 		`))
 	})
+
+	http.Handle("/template", &templateHandler{filename: "chat.html"})
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
